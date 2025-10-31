4d0dd11d82a90beb9a0b9d79732a6e46331e8a76
Runxi Yu <me@runxiyu.org>
2025-10-31 11:40:22 +0800

    More accessible requirements display on the selection page

--- frontend/src/pages/SelectionPage.vue
+++ frontend/src/pages/SelectionPage.vue
@@ -228,7 +228,6 @@ const requirementCounts = computed<
 				class="sticky top-[137px] z-10 bg-white border-b border-gray-200 px-8 py-4 flex justify-between items-center"
 			>
 				<div v-if="!isLoading" class="flex gap-3 text-base">
-					<span class="text-gray-600">Requirements:</span>
 					<template v-for="(req, i) in requirementCounts" :key="i">
 						<span v-if="i > 0" class="text-gray-300">·</span>
 						<span
@@ -237,8 +236,13 @@ const requirementCounts = computed<
 									? 'text-[#5bae31]'
 									: 'text-red-600'
 							"
-							>{{ req.selected }}/{{ req.required }}
-							{{ req.categories.join('/') }}</span
+							>{{ req.selected }} of {{ req.required }}
+							{{ req.categories.join('/') }}
+							({{
+								req.selected >= req.required
+									? 'Satisfied'
+									: 'Unsatisfied'
+							}})</span
 						>
 					</template>
 				</div>
